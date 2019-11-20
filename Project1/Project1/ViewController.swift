4bd1f9c77c0ecdbfbfef5ca090726246bc59ad45
Ayax <casarrubias2837@gmail.com>
2019-11-19 21:16:30 -0600

    Challenge

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UITableViewController {
         for item in items {
             if item.hasPrefix("nssl"){
                 pictures.append(item)
+                pictures.sort()
             }
         }
         print(pictures)
@@ -33,7 +34,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
+        cell.textLabel?.text = ("Picture \(indexPath.row) of \(pictures.count)")
         return cell
     }
 
