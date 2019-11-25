9297c5cc470ced15bcf07b10bc2de0b91d192c79
Ayax <casarrubias2837@gmail.com>
2019-11-25 15:32:50 -0600

    Advanced AutoLayout to Project2

---
+++ Project6a/Project2/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Ayax Alexis Casarrubias Rodríguez on 19/11/19.
+//  Copyright © 2019 Ayax Alexis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    var counter = 0
+    
+    override func viewDidLoad() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(callShare))
+        super.viewDidLoad()
+        countries.append("estonia")
+        countries.append("france")
+        countries.append("germany")
+        countries.append("ireland")
+        countries.append("italy")
+        countries.append("monaco")
+        countries.append("nigeria")
+        countries.append("poland")
+        countries.append("russia")
+        countries.append("spain")
+        countries.append("uk")
+        countries.append("us")
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        askQuestion()
+    }
+
+    func askQuestion(action: UIAlertAction! =  nil){
+        countries.shuffle()
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        correctAnswer = Int.random(in: 0...2)
+        title = "\(countries[correctAnswer].uppercased()) score: \(score)"
+    }
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if sender.tag == correctAnswer{
+            title = "Correct"
+            score += 1
+            counter += 1
+        }else{
+            title = "Wrong"
+            score -= 1
+            counter += 1
+        }
+        if counter <= 10{
+            let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+        }else{
+            let ac = UIAlertController(title: title, message: "Your final score is \(score).", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Restart", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+            score = 0
+            counter = 0
+        }
+    }
+    
+    @objc func callShare() {
+        let vc = UIActivityViewController(activityItems: [score], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+}
+
