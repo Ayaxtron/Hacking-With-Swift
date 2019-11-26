74b2a748130ae0aefca97a54f4c912dfc8213fe4
Ayax <casarrubias2837@gmail.com>
2019-11-25 18:32:42 -0600

    Project7
    Retrieve information from JSON's in the web, convert data using Codable protocol and display them using html.

---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Ayax Alexis Casarrubias Rodríguez on 25/11/19.
+//  Copyright © 2019 Ayax Alexis. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable{
+    var results: [Petition]
+}
