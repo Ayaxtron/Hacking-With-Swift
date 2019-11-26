9a51af034efce2db106cb86a6237dafc8aeddc55
Ayax <casarrubias2837@gmail.com>
2019-11-26 09:51:07 -0600

    Project9
    GCD Threads and QoS

---
+++ Project9/Project7/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Ayax Alexis Casarrubias Rodríguez on 25/11/19.
+//  Copyright © 2019 Ayax Alexis. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable{
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
+
