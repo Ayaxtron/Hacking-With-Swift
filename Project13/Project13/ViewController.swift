7c992745e50e0e784e368d7f659b37085e36991e
Ayax <casarrubias2837@gmail.com>
2019-12-10 23:42:58 -0600

    Project13 setup

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Ayax Alexis Casarrubias Rodríguez on 10/12/19.
+//  Copyright © 2019 Ayax Alexis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet var imageView: UIImageView!
+    @IBAction func intensity(_ sender: Any) {
+    }
+    
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+}
+
