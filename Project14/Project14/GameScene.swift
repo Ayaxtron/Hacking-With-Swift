23fa55752e2640890cfba619dd8381d85e6bf0a5
Ayax <casarrubias2837@gmail.com>
2020-01-03 23:40:13 -0600

    Project14
    Just Setup

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Ayax Alexis Casarrubias Rodríguez on 03/01/20.
+//  Copyright © 2020 Ayax Alexis. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+      
+    }
+    
+}
