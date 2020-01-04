120d5376a8d75096e6f7e6917ad8ef3f80eece3e
Ayax <casarrubias2837@gmail.com>
2020-01-04 15:40:37 -0600

    Project14
    Wack-A-Penguin

--- Project14/Project14/GameScene.swift
+++ Project14/Project14/GameScene.swift
@@ -11,13 +11,69 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var slots = [WhackSlot]()
+    var gameScore: SKLabelNode!
+    var score = 0{
+        didSet{
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    var popupTime = 0.85
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "whackBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
         
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.position = CGPoint(x: 8, y: 8)
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
+        
+        //Position of the holes and penguins
+        for i in 0 ..< 5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 410))}
+        for i in 0 ..< 4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320))}
+        for i in 0 ..< 5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230))}
+        for i in 0 ..< 4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140))}
+        //Call the method for the first time with a 1 second delay, after that the method will just call on itself
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.createEnemy()
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
       
+    }
+    //This gets called and creates a WackSlot
+    func createSlot(at position: CGPoint){
+        let slot = WhackSlot()
+        slot.configure(at: position)
+        addChild(slot)
+        slots.append(slot)
+    }
+    
+    func createEnemy(){
+        popupTime *= 0.991
+        //Shuffles and makes the first slot show itself
+        slots.shuffle()
+        slots[0].show(hideTime: popupTime)
+        //Generate some random numbers for some slots to show
+        if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime)}
+        if Int.random(in: 0...12) > 8 { slots[2].show(hideTime: popupTime)}
+        if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime)}
+        if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popupTime)}
+        //Create a new random delay
+        let minDelay = popupTime / 2.0
+        let maxDelay = popupTime * 2
+        let delay = Double.random(in: minDelay...maxDelay)
+        //Use the delay to call the same method all over again
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+            self?.createEnemy()
+        }
     }
     
 }
---
+++ Project14/Project14/WhackSlot.swift
@@ -0,0 +1,62 @@
+//
+//  WhackSlot.swift
+//  Project14
+//
+//  Created by Ayax Alexis Casarrubias Rodríguez on 04/01/20.
+//  Copyright © 2020 Ayax Alexis. All rights reserved.
+//
+import SpriteKit
+import UIKit
+
+class WhackSlot: SKNode {
+
+    var charNode: SKSpriteNode!
+    var isVisible = false
+    var isHit = false
+    
+    func configure(at position: CGPoint){
+        self.position = position
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        //Create the mask used to hide the penguins
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        //Add the penguin to the cropNode
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        //Add the cropNode to the slot
+        addChild(cropNode)
+    }
+    
+    func show(hideTime: Double){
+        if isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        isVisible = true
+        isHit = false
+        //Create bad and good penguins randomly
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        //Hide penguin after a certain amount of time
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) { [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
+    }
+    
+}
