4053f716b3dbe1e80db04ed06e29c74b625771ee
Ayax <casarrubias2837@gmail.com>
2020-01-11 22:46:39 -0600

    update

--- Project17/Project17/GameScene.swift
+++ Project17/Project17/GameScene.swift
@@ -82,6 +82,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if !isGameOver {
             score += 1
         }
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
