fa94ee00871a6c47ee7bff6b2398828a9f7923da
Ayax <casarrubias2837@gmail.com>
2020-01-12 23:17:43 -0600

    Update
    Debugging using Breakpoints

--- Project18/Project18/ViewController.swift
+++ Project18/Project18/ViewController.swift
@@ -24,9 +24,20 @@ class ViewController: UIViewController {
         
         //Assertions are debug-only checks (they will only execute while debugging, and Xcode will disable them whem the app is released) that will force the app to crash if a specific condition isn´t true.
         
-        assert(1 == 2, "Maths failure!")
+        assert(1 == 1, "Maths failure!")
         //It receives two parameters, something to check and a message to print if the check fails.
         assert(myReallySlowMethod() == true, "The slow method returned false, which is a bad thing!")
+        
+        //Breackpoints
+        
+        for i in 1...100 {
+            //Xcode will pause the execution of code at the breackpoint and show the value of all your variables
+            print("Number is \(i)") //The window in the right shows the threads and who call who
+            //In the console you can write p i to print the current value of i
+            //We can also make breackpoints conditionals with Edit Breakpoint
+            
+            //We can also trigger breackpoint when an Exception is thrown, o make this happen, press Cmd+8 to choose the breakpoint navigator – it's on the left of your screen, where the project navigator normally sits. Now click the + button in the bottom-left corner and choose "Exception Breakpoint."
+        }
     }
     
     
