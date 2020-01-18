7484d3e9a9b2f7a0c6dede8a1a2b82366673899f
Ayax <casarrubias2837@gmail.com>
2020-01-18 13:21:27 -0600

    Challenge7
    For later

---
+++ Challenge7/Challenge7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenge7
+//
+//  Created by Ayax Alexis Casarrubias Rodríguez on 18/01/20.
+//  Copyright © 2020 Ayax Alexis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
