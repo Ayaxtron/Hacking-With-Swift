7aa34c78eada4567111ccd41058557324340e8fc
Ayax <casarrubias2837@gmail.com>
2020-02-08 13:49:37 -0600

    update

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -9,14 +9,22 @@ import CoreMotion
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
+    var isGameOver = false
     var player: SKSpriteNode!
     var lastTouchPosition: CGPoint?
     var motionManager: CMMotionManager!
+    var scoreLabel: SKLabelNode!
+    
+    var score = 0{
+        didSet{
+            scoreLabel.text = "Score \(score)"
+        }
+    }
     
     enum CollisionTypes: UInt32 {
         case player = 1
@@ -29,17 +37,37 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         physicsWorld.gravity = .zero
         
+        physicsWorld.contactDelegate = self
+        
         let background = SKSpriteNode(imageNamed: "background.jpg")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
         
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score 0"
+        scoreLabel.horizontalAlignmentMode = .left
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.zPosition = 2
+        addChild(scoreLabel)
+        
         motionManager = CMMotionManager()
         motionManager.startAccelerometerUpdates()
         
         loadLevel()
         createPlayer()
+    }
+    //To know when there's contact with our player, and who cause it could be either player collided with the star or star collided with player
+    func didBegin(_ contact: SKPhysicsContact) {
+        guard let nodeA = contact.bodyA.node else { return }
+        guard let nodeB = contact.bodyB.node else { return }
+        
+        if nodeA == player {
+            playerCollided(with: nodeB)
+        }else if nodeB == player {
+            playerCollided(with: nodeA)
+        }
     }
     
     func loadLevel() {
@@ -144,6 +172,8 @@ class GameScene: SKScene {
     }
     
     override func update(_ currentTime: TimeInterval) {
+        guard isGameOver == false else { return }
+        
         //The next code is to test the code either in the simulation or the live device
         #if targetEnvironment(simulator) //The {} are not necessary
             //Calculate the difference between the current touch and the player's position, then use that to change the gravity value of the physics world.
@@ -156,6 +186,30 @@ class GameScene: SKScene {
             physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.x * -50, dy: accelerometerData.acceleration.y * 50)
         }
         #endif
+    }
+    
+    func playerCollided(with node: SKNode) {
+        //Make the ball disappear with a sequence
+        if node.name == "vortex" {
+            player.physicsBody?.isDynamic = false
+            isGameOver = true
+            score -= 1
+            //Move ball into vortex to look as if it's disappearing
+            let move = SKAction.move(to: node.position, duration: 0.25)
+            let scale = SKAction.scale(to: 0.0001, duration: 0.25)
+            let remove = SKAction.removeFromParent()
+            let sequence = SKAction.sequence([move,scale,remove])
+            //Once the sequence is done, the closure will create a new player
+            player.run(sequence) { [weak self] in
+                self?.createPlayer()
+                self?.isGameOver = false
+            }
+        } else if node.name == "star" {
+            node.removeFromParent()
+            score += 1
+        } else if node.name == "finish" {
+            // next level?
+        }
     }
     /*
     func touchDown(atPoint pos : CGPoint) {
